73eefc0898b121c87a9da63bceef36ae28dd4297
Isabelle Villasenor <villasenori@outlook.com>
2021-01-04 11:31:04 -0700

    Add Actor Create Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :studios, only: [:index] 
-  resources :movies, only: [:show]
+  
+  resources :movies, only: [:show] do
+    resources :actors, only: [:create]
+  end
  
 end
