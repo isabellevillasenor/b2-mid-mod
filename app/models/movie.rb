e0dfd14d63454ef350ff74c296b35833c0d23a67
Isabelle Villasenor <villasenori@outlook.com>
2021-01-04 10:49:42 -0700

    Add avg_act and act_age to Movie Model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,13 @@
 class Movie < ApplicationRecord
-  has_many :movie_actors
+  has_many :movie_actors, dependent: :destroy
   has_many :actors, through: :movie_actors
   belongs_to :studio
+
+  def act_age
+    actors.order(:age)
+  end
+
+  def avg_act
+    actors.average(:age).round(1)
+  end
 end
