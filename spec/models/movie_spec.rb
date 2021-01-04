135faf80696f9056c1db911d96019933988bed9d
Isabelle Villasenor <villasenori@outlook.com>
2021-01-04 10:24:17 -0700

    Add Relationship to Movie Model and Spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Movie do
   describe 'relationships' do 
+    it { should have_many :movie_actors}
+    it { should have_many(:actors).through(:movie_actors) }
     it { should belong_to :studio }
   end
 end
