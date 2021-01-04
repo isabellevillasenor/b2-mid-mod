1cf5012640637fa351dec4b4be81afe0cd290a6c
Isabelle Villasenor <villasenori@outlook.com>
2021-01-04 10:50:10 -0700

    Add dependent: :destroy to Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Studio.destroy_all
 Movie.destroy_all
+Actor.destroy_all
+MovieActor.destroy_all
 
 s1 = Studio.create!(name: 'Studio 1', location: 'Downtown Denver')
 s2 = Studio.create!(name: 'Studio 2', location: 'Cap Hill')
@@ -9,3 +11,19 @@ m2 = s1.movies.create!(title: 'Harry Potter Prison of Azkaban', creation_year: 2
 m3 = s2.movies.create!(title: 'Star Wars Empire Strikes Back', creation_year: 1980, genre: 'Sci-Fi')
 m4 = s2.movies.create!(title: 'Star Wars Return of the Jedi', creation_year: 1983, genre: 'Sci-Fi')
 m5 = s2.movies.create!(title: 'Star Wars A New Hope', creation_year: 1977, genre: 'Sci-Fi')
+
+a1 = Actor.create!(name: 'Actor 1', age: 16)
+a2 = Actor.create!(name: 'Actor 2', age: 48)
+a3 = Actor.create!(name: 'Actor 3', age: 25)
+a4 = Actor.create!(name: 'Actor 4', age: 32)
+a5 = Actor.create!(name: 'Actor 5', age: 22)
+
+
+MovieActor.create!(movie: m1, actor: a1)
+MovieActor.create!(movie: m1, actor: a2)
+MovieActor.create!(movie: m1, actor: a3)
+MovieActor.create!(movie: m2, actor: a4)
+MovieActor.create!(movie: m2, actor: a5)
+MovieActor.create!(movie: m3, actor: a1)
+MovieActor.create!(movie: m4, actor: a2)
+MovieActor.create!(movie: m5, actor: a3)
